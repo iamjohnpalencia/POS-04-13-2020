734f1ab57c072b37e6fa4db9243ad71aac7a2e79
Aiolos <47620043+iamjohnpalencia@users.noreply.github.com>
2020-08-14 19:42:44 +0800

    Commit

--- POS/SUBFORMS/About.Designer.vb
+++ POS/SUBFORMS/About.Designer.vb
@@ -26,12 +26,12 @@ Partial Class About
         Me.Label12 = New System.Windows.Forms.Label()
         Me.Label17 = New System.Windows.Forms.Label()
         Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.LabelVersion = New System.Windows.Forms.Label()
         Me.PictureBox5 = New System.Windows.Forms.PictureBox()
         Me.PictureBox4 = New System.Windows.Forms.PictureBox()
         Me.PictureBox3 = New System.Windows.Forms.PictureBox()
         Me.PictureBox2 = New System.Windows.Forms.PictureBox()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
-        Me.LabelVersion = New System.Windows.Forms.Label()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Panel1.SuspendLayout()
         CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
@@ -43,29 +43,29 @@ Partial Class About
         '
         'Label12
         '
-        Me.Label12.AutoSize = True
         Me.Label12.Font = New System.Drawing.Font("Tahoma", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label12.ForeColor = System.Drawing.Color.FromArgb(CType(CType(45, Byte), Integer), CType(CType(52, Byte), Integer), CType(CType(54, Byte), Integer))
         Me.Label12.ImeMode = System.Windows.Forms.ImeMode.NoControl
-        Me.Label12.Location = New System.Drawing.Point(0, 176)
+        Me.Label12.Location = New System.Drawing.Point(13, 176)
         Me.Label12.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label12.Name = "Label12"
-        Me.Label12.Size = New System.Drawing.Size(529, 35)
+        Me.Label12.Size = New System.Drawing.Size(601, 35)
         Me.Label12.TabIndex = 107
         Me.Label12.Text = "Innovention Food Asia Corporation"
+        Me.Label12.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
         'Label17
         '
-        Me.Label17.AutoSize = True
         Me.Label17.Font = New System.Drawing.Font("Tahoma", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label17.ForeColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
         Me.Label17.ImeMode = System.Windows.Forms.ImeMode.NoControl
-        Me.Label17.Location = New System.Drawing.Point(121, 223)
+        Me.Label17.Location = New System.Drawing.Point(19, 223)
         Me.Label17.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label17.Name = "Label17"
-        Me.Label17.Size = New System.Drawing.Size(262, 16)
+        Me.Label17.Size = New System.Drawing.Size(595, 16)
         Me.Label17.TabIndex = 105
         Me.Label17.Text = "© 2019 - Innovention Food Asia Corporation"
+        Me.Label17.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
         'Panel1
         '
@@ -78,16 +78,28 @@ Partial Class About
         Me.Panel1.Controls.Add(Me.Label12)
         Me.Panel1.Controls.Add(Me.Label17)
         Me.Panel1.Controls.Add(Me.Label1)
-        Me.Panel1.Location = New System.Drawing.Point(426, 154)
+        Me.Panel1.Location = New System.Drawing.Point(409, 154)
         Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(525, 339)
+        Me.Panel1.Size = New System.Drawing.Size(628, 339)
         Me.Panel1.TabIndex = 114
+        '
+        'LabelVersion
+        '
+        Me.LabelVersion.Font = New System.Drawing.Font("Tahoma", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LabelVersion.ForeColor = System.Drawing.Color.FromArgb(CType(CType(45, Byte), Integer), CType(CType(52, Byte), Integer), CType(CType(54, Byte), Integer))
+        Me.LabelVersion.Location = New System.Drawing.Point(10, 207)
+        Me.LabelVersion.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.LabelVersion.Name = "LabelVersion"
+        Me.LabelVersion.Size = New System.Drawing.Size(614, 16)
+        Me.LabelVersion.TabIndex = 115
+        Me.LabelVersion.Text = "Version 1.0.0"
+        Me.LabelVersion.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
         'PictureBox5
         '
         Me.PictureBox5.BackgroundImage = CType(resources.GetObject("PictureBox5.BackgroundImage"), System.Drawing.Image)
         Me.PictureBox5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-        Me.PictureBox5.Location = New System.Drawing.Point(332, 259)
+        Me.PictureBox5.Location = New System.Drawing.Point(380, 259)
         Me.PictureBox5.Name = "PictureBox5"
         Me.PictureBox5.Size = New System.Drawing.Size(54, 50)
         Me.PictureBox5.TabIndex = 120
@@ -97,7 +109,7 @@ Partial Class About
         '
         Me.PictureBox4.BackgroundImage = CType(resources.GetObject("PictureBox4.BackgroundImage"), System.Drawing.Image)
         Me.PictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-        Me.PictureBox4.Location = New System.Drawing.Point(272, 259)
+        Me.PictureBox4.Location = New System.Drawing.Point(320, 259)
         Me.PictureBox4.Name = "PictureBox4"
         Me.PictureBox4.Size = New System.Drawing.Size(54, 50)
         Me.PictureBox4.TabIndex = 119
@@ -107,7 +119,7 @@ Partial Class About
         '
         Me.PictureBox3.BackgroundImage = CType(resources.GetObject("PictureBox3.BackgroundImage"), System.Drawing.Image)
         Me.PictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-        Me.PictureBox3.Location = New System.Drawing.Point(212, 259)
+        Me.PictureBox3.Location = New System.Drawing.Point(260, 259)
         Me.PictureBox3.Name = "PictureBox3"
         Me.PictureBox3.Size = New System.Drawing.Size(54, 50)
         Me.PictureBox3.TabIndex = 118
@@ -117,7 +129,7 @@ Partial Class About
         '
         Me.PictureBox2.BackgroundImage = CType(resources.GetObject("PictureBox2.BackgroundImage"), System.Drawing.Image)
         Me.PictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-        Me.PictureBox2.Location = New System.Drawing.Point(153, 259)
+        Me.PictureBox2.Location = New System.Drawing.Point(201, 259)
         Me.PictureBox2.Name = "PictureBox2"
         Me.PictureBox2.Size = New System.Drawing.Size(54, 50)
         Me.PictureBox2.TabIndex = 117
@@ -127,34 +139,22 @@ Partial Class About
         '
         Me.PictureBox1.BackgroundImage = CType(resources.GetObject("PictureBox1.BackgroundImage"), System.Drawing.Image)
         Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
-        Me.PictureBox1.Location = New System.Drawing.Point(75, 3)
+        Me.PictureBox1.Location = New System.Drawing.Point(130, 3)
         Me.PictureBox1.Name = "PictureBox1"
         Me.PictureBox1.Size = New System.Drawing.Size(367, 176)
         Me.PictureBox1.TabIndex = 114
         Me.PictureBox1.TabStop = False
         '
-        'LabelVersion
-        '
-        Me.LabelVersion.AutoSize = True
-        Me.LabelVersion.Font = New System.Drawing.Font("Tahoma", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.LabelVersion.ForeColor = System.Drawing.Color.FromArgb(CType(CType(45, Byte), Integer), CType(CType(52, Byte), Integer), CType(CType(54, Byte), Integer))
-        Me.LabelVersion.Location = New System.Drawing.Point(216, 207)
-        Me.LabelVersion.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
-        Me.LabelVersion.Name = "LabelVersion"
-        Me.LabelVersion.Size = New System.Drawing.Size(84, 16)
-        Me.LabelVersion.TabIndex = 115
-        Me.LabelVersion.Text = "Version 1.0.0"
-        '
         'Label1
         '
-        Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Tahoma", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
-        Me.Label1.Location = New System.Drawing.Point(203, 240)
+        Me.Label1.Location = New System.Drawing.Point(22, 240)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(119, 16)
+        Me.Label1.Size = New System.Drawing.Size(592, 16)
         Me.Label1.TabIndex = 116
         Me.Label1.Text = "All Rights Reserve. "
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
         'About
         '
@@ -168,7 +168,6 @@ Partial Class About
         Me.Text = "About"
         Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
         Me.Panel1.ResumeLayout(False)
-        Me.Panel1.PerformLayout()
         CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
