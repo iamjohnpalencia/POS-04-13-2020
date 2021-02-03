272863d3b0c80f2b06d0dec54b956e82f8dbf828
Aiolos <47620043+iamjohnpalencia@users.noreply.github.com>
2021-02-03 10:52:04 +0800

    C

--- POS/SUBFORMS/SeniorDetails.Designer.vb
+++ POS/SUBFORMS/SeniorDetails.Designer.vb
@@ -53,9 +53,9 @@ Partial Class SeniorDetails
         Me.Label1.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label1.Location = New System.Drawing.Point(3, 5)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(26, 19)
+        Me.Label1.Size = New System.Drawing.Size(163, 19)
         Me.Label1.TabIndex = 104
-        Me.Label1.Text = "ID"
+        Me.Label1.Text = "Identification Number"
         '
         'TextBoxSENIORID
         '
